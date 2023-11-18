e93171d1b5c51a9ab874645284346c9b98b96d08
matijasevicstipe2 <smatijasevic57@gmail.com>
2023-11-18 04:06:59 +0100

    Initial commit

---
+++ lib/book_parser.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+def extract_books_after_year(file_path, year)
+  books = []
+  CSV.foreach(file_path, headers: true) do |row|
+    title = row['title']
+    author = row['author']
+    publication_year = row['publication_year'].to_i
+    if publication_year > year
+      books << { title: title, author: author, publication_year: publication_year }
+    end
+  end
+  books.sort_by! { |book| book[:publication_year] }
+
+  books.each do |book|
+    puts "#{book[:title]} (#{book[:publication_year]}) by #{book[:author]}"
+  end
+end
+
+# Example usage:
+file_path = 'books.csv'
+year = 2010
+extract_books_after_year(file_path, year)
