c881505c945c3d9b0811b841b06035e6f771f3fd
matijasevicstipe2 <smatijasevic57@gmail.com>
2023-11-20 03:16:33 +0100

    commit #4

--- lib/main.rb
+++ lib/main.rb
@@ -3,7 +3,7 @@
 require_relative 'book_parser'
 
 def main(year = nil)
-  file_path = 'lib/data/books.csv'
+  file_path = 'data/books.csv'
   if year.nil?
     loop do
       print 'Enter the year (or "q" to quit): '
@@ -21,6 +21,10 @@ def main(year = nil)
 end
 
 # Example usage:
-main
-#main(2010)
+if ARGV.empty?
+  main
+else
+  year = ARGV[0].to_i
+  main(year)
+end
 
